f48b7b1b22fd85473565fbb9ed1b62173a734a50
forgehe <liao098@gmail.com>
2023-04-06 11:53:11 -0700

    add 2nd form for unpublished decks

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -16,6 +16,20 @@ class ProxyController < ActionController::Base
     HTTParty.get(decklist_api + deck_id)["slots"].reject {|id, quantity| id == "01000"}
   end
 
+  def deck
+    /(?<id>\d+)/ =~ params[:id]
+    cards = deck_card_ids(id).transform_keys { |card_id| card_image_url(card_id) }
+    send_data PdfGenerator.generate(cards), filename: "cards.pdf"
+  rescue
+    flash.alert = "Deck not found"
+    render :show
+  end
+
+  def deck_card_ids(deck_id)
+    decklist_api = "https://arkhamdb.com/api/public/deck/"
+    HTTParty.get(decklist_api + deck_id)["slots"].reject {|id, quantity| id == "01000"}
+  end
+
   def card_image_url(card_id)
     card_api = "https://arkhamdb.com/api/public/card/"
     "https://arkhamdb.com" + HTTParty.get(card_api + card_id)["imagesrc"]
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get 'proxy#send_data', to: 'proxy#show'
   get '/deck', to: 'proxy#new'
   get '/deck/:id', to: 'proxy#new'
+  get '/udeck', to: 'proxy#deck'
+  get '/udeck/:id', to: 'proxy#deck'
   get '/card', to: 'proxy#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
