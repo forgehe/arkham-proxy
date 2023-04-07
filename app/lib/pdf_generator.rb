2a66b46295a5f6bc25ae6352b581bca9171fcb82
forgehe <liao098@gmail.com>
2023-04-07 05:22:13 -0700

    adjust margin and positioning

--- app/lib/pdf_generator.rb
+++ app/lib/pdf_generator.rb
@@ -2,7 +2,7 @@ require "prawn/measurement_extensions"
 
 module PdfGenerator
   def self.generate(cards)
-    pdf = Prawn::Document.new(:page_size => "A4", :page_layout => :portrait, print_scaling: :none,)
+    pdf = Prawn::Document.new(:page_size => "A4", :page_layout => :portrait, print_scaling: :none, margin: 0)
     reset_cursor
     cards.each do |card_image_url, quantity|
       begin
@@ -18,9 +18,9 @@ module PdfGenerator
   def self.add_image_to_pdf(image, pdf)
     pdf.image image, width: 2.5.in, at: [@@x_position, @@y_position]
     if ((@@x_position += 2.5.in) > 5.in)
-      @@x_position = 0
+      @@x_position = 0.5in
       if ((@@y_position -= 3.5.in) < 1.5.in)
-        @@y_position = 11.5.in
+        @@y_position = 11.in
         pdf.start_new_page
       end
     end
@@ -35,7 +35,7 @@ module PdfGenerator
 
   def self.reset_cursor
     # @@y_position = 520
-    @@y_position = 11.5.in
-    @@x_position = 0
+    @@y_position = 11.in
+    @@x_position = 0.5in
   end
 end
