38ca3288cfec7bbe4dd9c99afed542507eec05c1
forgehe <liao098@gmail.com>
2023-04-07 05:53:03 -0700

    add single box deck add and adjust pdf sizing

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -4,16 +4,17 @@ class ProxyController < ActionController::Base
 
   def new
     /(?<id>\d+)/ =~ params[:id]
-    cards = card_ids(id).transform_keys { |card_id| card_image_url(card_id) }
+    /(?<deckType>decklist|deck)/i =~ params[:id]
+    cards = card_ids(id, deckType).transform_keys { |card_id| card_image_url(card_id) }
     send_data PdfGenerator.generate(cards), filename: "cards.pdf"
   rescue
     flash.alert = "Deck not found"
     render :show
   end
 
-  def card_ids(deck_id)
-    decklist_api = "https://arkhamdb.com/api/public/decklist/"
-    HTTParty.get(decklist_api + deck_id)["slots"].reject {|id, quantity| id == "01000"}
+  def card_ids(deck_id, deck_type)
+    decklist_api = "https://arkhamdb.com/api/public/"
+    HTTParty.get(decklist_api + deck_type + deck_id)["slots"].reject {|id, quantity| id == "01000"}
   end
 
   def deck
--- app/lib/pdf_generator.rb
+++ app/lib/pdf_generator.rb
@@ -2,7 +2,7 @@ require "prawn/measurement_extensions"
 
 module PdfGenerator
   def self.generate(cards)
-    pdf = Prawn::Document.new(:page_size => "A4", :page_layout => :portrait, print_scaling: :none, margin: 0)
+    pdf = Prawn::Document.new(:page_size => "LETTER", :page_layout => :portrait, print_scaling: :none, margin: 0)
     reset_cursor
     cards.each do |card_image_url, quantity|
       begin
@@ -17,10 +17,10 @@ module PdfGenerator
 
   def self.add_image_to_pdf(image, pdf)
     pdf.image image, width: 2.5.in, at: [@@x_position, @@y_position]
-    if ((@@x_position += 2.5.in) > 5.in)
+    if ((@@x_position += 2.5.in) > 6.in)
       @@x_position = 0.5.in
       if ((@@y_position -= 3.5.in) < 1.5.in)
-        @@y_position = 11.in
+        @@y_position = 10.75.in
         pdf.start_new_page
       end
     end
@@ -35,7 +35,7 @@ module PdfGenerator
 
   def self.reset_cursor
     # @@y_position = 520
-    @@y_position = 11.in
+    @@y_position = 10.75.in
     @@x_position = 0.5.in
   end
 end
