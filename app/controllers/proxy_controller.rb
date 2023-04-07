e484aadea64e100d192ade5942378c1946a79ae9
forgehe <liao098@gmail.com>
2023-04-07 07:57:25 -0700

    front end template

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -4,6 +4,8 @@ class ProxyController < ActionController::Base
 
   def new
     data = params[:id].match(/(?<deckType>decklist|deck)\/view\/(?<id>\d{2,9})\/?(?<name>.*)/i)
+    p data
+    p data[:name]
     cards = card_ids(data[:id], data[:deckType]).transform_keys { |card_id| card_image_url(card_id) }
     filename = data[:deckType].casecmp("decklist") ? data[:name] : data[:id]
     send_data PdfGenerator.generate(cards), filename: "#{filename}.pdf"
