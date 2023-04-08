0ea7ac8ece2eeabd2046e7a2b70603a76823fe62
forgehe <liao098@gmail.com>
2023-04-08 15:52:51 -0700

    remove old routes, update form

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   root 'proxy#show'
   get 'proxy#send_data', to: 'proxy#show'
-  get '/deck', to: 'proxy#new'
-  get '/deck/:id', to: 'proxy#new'
-  get '/udeck', to: 'proxy#deck'
-  get '/udeck/:id', to: 'proxy#deck'
-  get '/card', to: 'proxy#new'
+  post '/deck', to: 'proxy#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
