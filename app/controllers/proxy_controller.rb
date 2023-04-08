fc01b38d9c0e1c73117873725469f1562051f4c2
forgehe <liao098@gmail.com>
2023-04-08 15:13:40 -0700

    add name debug, sideboard fix

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -4,10 +4,13 @@ class ProxyController < ActionController::Base
 
   def new
     data = params[:id].match(/(?<deckType>decklist|deck)\/view\/(?<id>\d{2,9})\/?(?<name>.*)/i)
-    p data
+    p "data"
     p data[:name]
     cards = card_ids(data[:id], data[:deckType]).transform_keys { |card_id| card_image_url(card_id) }
     filename = data[:deckType].casecmp("decklist") ? data[:name] : data[:id]
+    p "filename"
+    p filename
+    p data[:deckType].casecmp("decklist")
     send_data PdfGenerator.generate(cards), filename: "#{filename}.pdf"
   rescue
     flash.alert = "Deck not found, is it set to private?"
@@ -16,9 +19,12 @@ class ProxyController < ActionController::Base
 
   def card_ids(deck_id, deck_type)
     data = HTTParty.get("https://arkhamdb.com/api/public/#{deck_type}/#{deck_id}")
-    output = data["slots"].merge(data["sideSlots"]).reject {|id, quantity| id == "01000"}
+    output = data["slots"]
+    if data["sideSlots"]
+      output.merge(data["sideSlots"]){|_,x,y| x + y}
+    end
     p output
-    output
+    output.reject {|id, quantity| id == "01000"}
     # HTTParty.get("https://arkhamdb.com/api/public/#{deck_type}/#{deck_id}")["slots"].reject {|id, quantity| id == "01000"}
   end
 
