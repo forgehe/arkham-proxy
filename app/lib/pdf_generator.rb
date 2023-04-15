13599ca1e57a11cbd6742bd9b20ccbf1cbf5ba1a
forgehe <liao098@gmail.com>
2023-04-15 04:44:17 -0700

    move image moving to prawn

--- app/lib/pdf_generator.rb
+++ app/lib/pdf_generator.rb
@@ -1,22 +1,18 @@
 require "prawn/measurement_extensions"
+require 'open-uri'
 
 module PdfGenerator
   def self.generate(cards)
     pdf = Prawn::Document.new(:page_size => "LETTER", :page_layout => :portrait, print_scaling: :none, margin: 0)
     reset_cursor
     cards.each do |card_image_url, quantity|
-      begin
-        image = URI.open(card_image_url)
-      rescue OpenURI::HTTPError
-        next
-      end
-      quantity.times { add_image_to_pdf(image, pdf) }
+      quantity.times { add_image_to_pdf(card_image_url, pdf) }
     end
     pdf.render
   end
 
-  def self.add_image_to_pdf(image, pdf)
-    pdf.image image, width: 2.5.in, at: [@@x_position, @@y_position]
+  def self.add_image_to_pdf(img_url, pdf)
+    pdf.image URI.open(img_url), width: 2.5.in, at: [@@x_position, @@y_position]
     if ((@@x_position += 2.5.in) > 6.in)
       @@x_position = 0.5.in
       if ((@@y_position -= 3.5.in) < 1.5.in)
